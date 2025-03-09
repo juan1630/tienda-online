25b0ef62812c9033792c5689fa1eeef81dcee2f1
Juan Patrón <josejuanpatron1630@gmail.com>
2025-03-09 12:32:08 -0600

    added port

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ app.use(bodyParser.json());
 app.use(cors())
 app.use(require("./routes/index"));
 
-app.get('/' , (req, resp) => {
-  return resp.status(200);
-})
-app.listen(3200, (error) => {
+app.listen(process.env.PORT, (error) => {
   if (error) console.log(error);
-  console.log("Running on port 3200");
+  console.log(`Running on port ${process.env.PORT}`);
 });
 
 const connectToDb = async () => {
@@ -29,3 +26,8 @@ const connectToDb = async () => {
 };
 
 connectToDb();
+
+
+app.get('/' , (req, resp) => {
+  return resp.status(200);
+})
